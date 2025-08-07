5d5680a9e162a13c996d07a68f1bc22236608594
adiyasaniastya <adiyasa1011@gmail.com>
2025-08-07 22:58:23 +0700

    add options, basic styling in to do list app

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 function main() {
-    function validateForm(params) {
+    const authorName = 'brewokzcuyy'
+    function validateForm() {
         const todoInput = document.getElementById('todo-input');
         const todoDate = document.getElementById('todo-date');
-        console.log('todoInput : ', todoInput.val())
+        console.log('todoInput 1: ', todoInput)
 
         if (todoInput === '' || todoDate === ''){
             alert('masih kosong')
         }
     }
     const addBtn = document.getElementById('todo-add')
-    addBtn.on('click',validateForm())
+    addBtn.onclick = validateForm()
+    // footer
+    const copyrightFooter = document.getElementById('copyright-footer')
+    const year = new Date().getFullYear()
+    copyrightFooter.innerHTML =  '©' + year +' ' + copyrightFooter.innerHTML + ' ' + `<b>${authorName}</b>`;
+    copyrightFooter.style.fontStyle = 'italic';
 
     let todoArray = [];
 };
+
+main();
